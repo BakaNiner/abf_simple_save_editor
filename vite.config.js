9398b181aaeee92a3388cf3051ef6406d66041dc
BakaNiner <7195542+BakaNiner@users.noreply.github.com>
2024-11-17 19:02:53 +0800

    Init

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Abiotic-Factor-Simple-Save-Editor/'
+  base: '/abf_simple_save_editor/'
 })
